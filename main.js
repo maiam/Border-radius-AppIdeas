f13fd7d2a159409a8e1c9cbd86cc0e579d5dd86a
Pedro Monteiro <pedrosergiomaia@gmail.com>
2022-09-07 23:36:29 -0300

    update

--- main.js
+++ main.js
@@ -5,8 +5,45 @@ const inputTopRight = document.getElementById('top-right')
 const inputBottomRight = document.getElementById('bottom-right')
 const inputBottomLeft = document.getElementById('bottom-left')
 const square = document.getElementById('square')
-const rangeBottom = document.getElementById('range-bottom');
+const inputCopy = document.querySelector('.input-copy');
+const btn = document.querySelector('.btn-copy');
+const pCopy = document.getElementById('p-copy');
+const resetBtn = document.querySelector('.reset-button');
 
+
+function limpaInputs(topLeft,topRight, bottomRight, bottomLeft) {
+    inputTopLeft.value = topLeft;
+    inputTopRight.value = topRight; 
+    inputBottomRight.value = bottomRight;
+    inputBottomLeft.value = bottomLeft;
+}
+
+function copyClipboard(element){
+
+    // Select the text field
+    element.select();
+    element.setSelectionRange(0, 99999);
+
+    // Copy the text inside the text field
+    navigator.clipboard.writeText(element.value);
+    pCopy.innerHTML = `Copiado para a Área de transferência.`;
+}
+
+function injetaValorNoInput(inputTopLeft,inputTopRight, inputBottomRight, inputBottomLeft) {
+    
+    let topLeft = inputTopLeft.value;
+    let topRight = inputTopRight.value;
+    let bottomRight = inputBottomRight.value;
+    let bottomLeft = inputBottomLeft.value;
+
+    inputCopy.value = 
+    "border-radius: " + topLeft + "px "
+    + topRight + "px "
+    + bottomRight + "px "
+    + bottomLeft + "px ";
+
+    return inputCopy.value;
+}
 
 document.body.addEventListener('click', e => {
     if(e.target.tagName!='INPUT') {
@@ -22,7 +59,7 @@ inputs.forEach(element => {
        topRight(element);
        bottomRight(element);
        bottomLeft(element);
-       
+
     })
 });
 
@@ -55,6 +92,7 @@ function limpaTooltips () {
     spans.forEach(span => {
 
         span.classList.add('visible');
+
     })
 }
 
@@ -64,6 +102,7 @@ function changeBorder (topLeft,topRight,bottomRight,bottomLeft) {
     square.style.borderTopRightRadius = topRight + 'px';
     square.style.borderBottomRightRadius = bottomRight + 'px';
     square.style.borderBottomLeftRadius = bottomLeft + 'px';
+
 }
 
 document.addEventListener('input', e => {
@@ -74,12 +113,28 @@ document.addEventListener('input', e => {
         inputBottomRight.value,
         inputBottomLeft.value);
     
-})
+    injetaValorNoInput(inputTopLeft,inputTopRight, inputBottomRight, inputBottomLeft);
 
+    
+})
 
 function toggleTooltip (text) {
     const span = document.getElementById(text)
     span.classList.toggle('visible');
+    pCopy.classList.add('visible');
 }
 
 
+btn.addEventListener('click', () => {
+
+   copyClipboard(inputCopy);
+   pCopy.classList.remove('visible');
+   console.log(inputCopy.value)
+})
+
+resetBtn.addEventListener('click', () => {
+
+    changeBorder(0,0,0,0);
+    limpaInputs(0,0,0,0);
+    pCopy.classList.add('visible');
+});
